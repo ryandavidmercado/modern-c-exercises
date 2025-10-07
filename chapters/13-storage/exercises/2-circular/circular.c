53a76f7e915091907112a72289fb8429f8ff24c2
Ryan Mercado <ryandavidmercado@gmail.com>
2025-10-07 12:57:26 -0400

    finish circular exercise

--- chapters/13-storage/exercises/2-circular/circular.c
+++ chapters/13-storage/exercises/2-circular/circular.c
@@ -1,4 +1,5 @@
 #include "circular.h"
+#include <string.h>
 
 circular *circular_new(size_t len);
 void circular_delete(circular *c);
@@ -48,6 +49,7 @@ double *circular_element(circular const *c, size_t pos) {
   return &c->tab[pos_circular];
 }
 
+[[deprecated("implementation")]]
 circular *circular_append(circular *c, double value) {
   if (!c || c->len >= c->cap) {
     return nullptr;
@@ -61,6 +63,7 @@ circular *circular_append(circular *c, double value) {
   return c;
 }
 
+[[deprecated("implementation")]]
 double circular_pop(circular *c) {
   if (!c || c->len == 0) {
     return 0.0;
@@ -73,4 +76,48 @@ double circular_pop(circular *c) {
   return ret;
 }
 
-size_t circular_getlength(circular const *c) { return c->len; }
+[[deprecated("implementation")]]
+size_t circular_getlength(circular const *c) {
+  return c->len;
+}
+
+[[deprecated("implementation")]] [[nodiscard(
+    "returned pointer replaces function argument")]]
+circular *circular_resize(circular *c, size_t nlen) {
+  if (!c || c->len > nlen)
+    return nullptr;
+
+  size_t len = c->len;
+  size_t olen = c->cap;
+  if (olen == nlen)
+    return c;
+
+  size_t ostart = circular_getpos(c, 0);
+  size_t nstart = ostart;
+
+  double *otab = c->tab;
+  double *ntab = realloc(c->tab, sizeof(double[nlen]));
+
+  if (!ntab)
+    return nullptr;
+
+  if (ostart + len > olen) {
+    size_t ulen = olen - ostart;
+    size_t llen = len - ulen;
+    if (llen <= (nlen - olen)) {
+      memcpy(ntab + olen, ntab, llen * sizeof(double));
+    } else {
+      nstart = nlen - ulen;
+      memmove(ntab + nstart, ntab + ostart, ulen * sizeof(double));
+    }
+  }
+
+  *c = (circular){
+      .cap = nlen,
+      .start = nstart,
+      .len = c->len,
+      .tab = ntab,
+  };
+
+  return c;
+}
